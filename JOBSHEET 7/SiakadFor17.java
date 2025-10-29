f25118e10a4bc94b34964464e7400cdbab3df8f4
sinyozr <sinyozr4@gmail.com>
2025-10-29 14:08:16 +0700

    Daspro

--- JOBSHEET 7/SiakadFor17.java
+++ JOBSHEET 7/SiakadFor17.java
@@ -10,6 +10,7 @@ public class SiakadFor17 {
 
         int jumlahMahasiswa = 0;
         int jumlahtidakLulus = 0;
+
         for (int i = 1; i <= 10; i++) {
 
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -18,22 +19,20 @@ public class SiakadFor17 {
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
-
             if (nilai < terendah) {
                 terendah = nilai;
             }
-
             if (nilai < 60) {
                 jumlahtidakLulus++;
             } else {
                 jumlahMahasiswa++;
             }
-            System.out.println("==========================================");
-            System.out.println("jumlah mahasiswa lulus: " + jumlahtidakLulus);
-            System.out.println("jumlah mahasiswa tidak lulus: " + jumlahtidakLulus);
-            System.out.println("Nilai tertinggi: " + tertinggi);
-            System.out.println("Nilai terendah: " + terendah);
+
         }
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah siswa lulus: " + jumlahMahasiswa);
+        System.out.println("Jumlah siswa tidak lulus: " + jumlahtidakLulus);
 
         sc.close();
     }
