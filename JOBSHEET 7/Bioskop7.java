6ea6826151c017af8fa5667c1a20b8948c87fcef
sinyozr <sinyozr4@gmail.com>
2025-10-29 14:17:26 +0700

    Daspro

--- JOBSHEET 7/Bioskop7.java
+++ JOBSHEET 7/Bioskop7.java
@@ -1,62 +1,49 @@
 
-import java.text.NumberFormat;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Bioskop7 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         final int HARGA_PER_TIKET = 50000;
         int totalTiketHarian = 0;
         double totalPenjualanHarian = 0.0;
-
-        Locale localeID = new Locale("id", "ID");
-        NumberFormat formatRupiah = NumberFormat.getCurrencyInstance(localeID);
-
-        System.out.println("--- PROGRAM KASIR BIOSKOP ---");
-        System.out.println("Harga per tiket: " + formatRupiah.format(HARGA_PER_TIKET));
-        System.out.println("=========================================");
-
         while (true) {
-            System.out.print("\nMasukkan jumlah tiket (atau 0 untuk selesai): ");
+            System.out.print("\nMasukkan jumlah tiket yang dibeli (0 untuk selesai): ");
             int jmlTiket = sc.nextInt();
 
             if (jmlTiket == 0) {
-                System.out.println("Menutup program dan menghitung total...");
                 break;
             }
 
-            if (jmlTiket < 0) {
-                System.out.println("Input tidak valid. Jumlah tiket tidak boleh negatif.");
-                continue;
-            }
-
             double hargaAwal = (double) jmlTiket * HARGA_PER_TIKET;
-            double diskon = 0.0;
-            double hargaSetelahDiskon = 0.0;
-
+            double diskonDecimal = 0.0;
+            double diskonPersen = 0.0;
             if (jmlTiket > 10) {
-                diskon = 0.15;
-                System.out.println("Mendapat diskon 15%!");
+                diskonDecimal = 0.15;
+                diskonPersen = 15.0;
             } else if (jmlTiket > 4) {
-                diskon = 0.10;
-                System.out.println("Mendapat diskon 10%!");
+                diskonDecimal = 0.10;
+                diskonPersen = 10.0;
             }
 
-            hargaSetelahDiskon = hargaAwal - (hargaAwal * diskon);
-            System.out.println("Total harga transaksi ini: " + formatRupiah.format(hargaSetelahDiskon));
+            double hargaSetelahDiskon = hargaAwal - (hargaAwal * diskonDecimal);
+
+            System.out.println("Jumlah tiket \t: " + jmlTiket);
+            System.out.println("Harga per tiket : Rp " + HARGA_PER_TIKET);
+            System.out.println("Diskon \t\t: " + diskonPersen + "%");
+            System.out.println("Total bayar \t: Rp " + hargaSetelahDiskon);
 
             totalTiketHarian += jmlTiket;
             totalPenjualanHarian += hargaSetelahDiskon;
         }
 
-        System.out.println("\n=========================================");
-        System.out.println("--- LAPORAN PENJUALAN HARIAN ---");
-        System.out.println("Total Tiket Terjual: " + totalTiketHarian + " tiket");
-        System.out.println("Total Penjualan: " + formatRupiah.format(totalPenjualanHarian));
-        System.out.println("=========================================");
+        System.out.println("\n====== Rekapitulasi Penjualan Hari Ini ======");
+        System.out.println("Total tiket terjual : " + totalTiketHarian);
+        System.out.println("Total penjualan \t: Rp " + totalPenjualanHarian);
+        System.out.println("\nTerima kasih telah menggunakan program ini!");
+        System.out.println("------------------------------------------");
+
         sc.close();
     }
 }
