f6255de1b17831fc534dd72a2c3fb24ca4f6b000
sinyozr <sinyozr4@gmail.com>
2025-10-29 10:33:36 +0700

    Penambahan

--- JOBSHEET 7/SiakadFor17.java
+++ JOBSHEET 7/SiakadFor17.java
@@ -7,6 +7,9 @@ public class SiakadFor17 {
 
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+
+        int jumlahMahasiswa = 0;
+        int jumlahtidakLulus = 0;
         for (int i = 1; i <= 10; i++) {
 
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -19,6 +22,15 @@ public class SiakadFor17 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai < 60) {
+                jumlahtidakLulus++;
+            } else {
+                jumlahMahasiswa++;
+            }
+            System.out.println("==========================================");
+            System.out.println("jumlah mahasiswa lulus: " + jumlahtidakLulus);
+            System.out.println("jumlah mahasiswa tidak lulus: " + jumlahtidakLulus);
             System.out.println("Nilai tertinggi: " + tertinggi);
             System.out.println("Nilai terendah: " + terendah);
         }
