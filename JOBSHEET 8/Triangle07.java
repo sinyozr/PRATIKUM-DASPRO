777e3ab36f827d65c292d0f3835a491e9c825b3a
sinyozr <sinyozr4@gmail.com>
2025-11-05 10:53:25 +0700

    Daaspro

---
+++ JOBSHEET 8/Triangle07.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Triangle07 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("masukan nilai n =");
+
+        int n = sc.nextInt();
+        int i = 0;
+
+        while (i <= n) {
+            int j = 0;
+            while (j < i) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
