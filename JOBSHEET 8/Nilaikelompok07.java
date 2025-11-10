0f9f769dd528ff9eba4e121b31a900d8b6fc2846
sinyozr <sinyozr4@gmail.com>
2025-11-10 20:09:11 +0700

    Daspro

--- JOBSHEET 8/Nilaikelompok07.java
+++ JOBSHEET 8/Nilaikelompok07.java
@@ -5,23 +5,32 @@ public class Nilaikelompok07 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double nilai = 0, totalNilai = 0, rataNilai = 0;
         int i = 1;
+        float totalNilai;
+        float rataNilai;
+        float rataTertinggi = 0;
+        int kelompokTertinggi = 0;
 
         while (i <= 6) {
-            System.out.println("Kelompok " + i);
+            System.out.println("\nKelompok " + i);
             totalNilai = 0;
-
             for (int j = 1; j <= 5; j++) {
-                System.out.print("Masukkan nilai ke-" + j + " = ");
-                nilai = sc.nextDouble();
+                System.out.print("Nilai dari Kelompok Penilai " + j + " : ");
+                float nilai = sc.nextFloat();
                 totalNilai += nilai;
             }
-
             rataNilai = totalNilai / 5;
-            System.out.println("Rata-rata nilai kelompok " + i + " = " + rataNilai);
+            System.out.println("Kelompok " + i + " : nilai rata-rata = " + rataNilai);
+            if (rataNilai > rataTertinggi) {
+                rataTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
+
             i++;
         }
+
+        System.out.println("\nKelompok dengan nilai rata-rata tertinggi adalah Kelompok " + kelompokTertinggi + " dengan nilai " + rataTertinggi);
+
         sc.close();
     }
 }
